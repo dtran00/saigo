2e8bda610bbf81428bedb26737aab5b229cc50e1
Deanna <dtran@enova.com>
2018-05-31 15:55:37 -0500

    models_v1

--- exercise-006-models/src/models/customer.go
+++ exercise-006-models/src/models/customer.go
@@ -8,14 +8,27 @@ import (
 
 // Customer ...
 type Customer struct {
-	ID        int
-	Email     string
-	FirstName string
-	LastName  string
-	BirthDate time.Time
+	ID        int       `json:"id" db:"customer_id"`
+	Email     string    `json:"email" db:"email"`
+	FirstName string    `json:"first_name" db:"first_name"`
+	LastName  string    `json:"last_name" db:"last_name"`
+	BirthDate time.Time `json:"birth_date" db:"birth_date"`
 	Orders    []*Order
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type dbStore struct {
+	db *sqlx.DB
+}
+
+type Store interface {
+	FindCustomerByEmail(db *sqlx.DB, email string) (*Customer, error)
+	FindCustomerByID(db *sqlx.DB, id int) (*Customer, error)
+	NewCustomer(db *sqlx.DB, email string, first_name string, last_name string, birth_date time.Time) (*Customer, error)
+	DeleteCustomer(db *sqlx.DB, id int) error
+	UpdateCustomer(db *sqlx.DB, u *Customer) error
+	AllCustomers(db *sqlx.DB) ([]*Customer, error)
 }
 
 // Refresh ...
@@ -24,31 +37,47 @@ func (c *Customer) Refresh(db *sqlx.DB) error {
 }
 
 // NewCustomer ...
-func NewCustomer(db *sqlx.DB, email string, first_name string, last_name string, birth_date time.Time) (*Customer, error) {
-	return nil, nil
+func (s *dbStore) NewCustomer(db *sqlx.DB, email string, first_name string, last_name string, birth_date time.Time) (*Customer, error) {
+	_, err := s.db.Exec("INSERT INTO Customers (email, first_name, last_name, birth_date) VALUES ($1, $2, $3, $4)", email, first_name, last_name, birth_date)
+	return_customer := Customer{}
+	err = s.db.Get(&return_customer, "SELECT * FROM customers WHERE email = $1", email)
+
+	return &return_customer, err
 }
 
 // DeleteCustomer ...
-func DeleteCustomer(db *sqlx.DB, id int) error {
-	return nil
+func (s *dbStore) DeleteCustomer(db *sqlx.DB, id int) error {
+	_, err := s.db.Exec("DELETE FROM Customers WHERE customer_id = $1", id)
+	return err
 }
 
 // UpdateCustomer ...
-func UpdateCustomer(db *sqlx.DB, u *Customer) error {
-	return nil
+func (s *dbStore) UpdateCustomer(db *sqlx.DB, u *Customer) error {
+	sqlStatement := `UPDATE Customers SET email = $2, first_name = $3, last_name = $4, birth_date = $5 WHERE customer_id = $1`
+	_, err := s.db.Exec(sqlStatement, u.ID, u.Email, u.FirstName, u.LastName, u.BirthDate)
+	return err
 }
 
 // FindCustomerByEmail ...
-func FindCustomerByEmail(db *sqlx.DB, email string) (*Customer, error) {
-	return nil, nil
+func (s *dbStore) FindCustomerByEmail(db *sqlx.DB, email string) (*Customer, error) {
+	return_customer := Customer{Email: email}
+	err := s.db.Get(&return_customer, "SELECT * FROM Customers WHERE email = $1", email)
+
+	return &return_customer, err
 }
 
 // FindCustomerByID ...
-func FindCustomerByID(db *sqlx.DB, id int) (*Customer, error) {
-	return nil, nil
+func (s *dbStore) FindCustomerByID(db *sqlx.DB, id int) (*Customer, error) {
+	return_customer := Customer{ID: id}
+	err := s.db.Get(&return_customer, "SELECT * FROM Customers WHERE customer_id = $1", id)
+
+	return &return_customer, err
 }
 
 // AllCustomers ...
-func AllCustomers(db *sqlx.DB) ([]*Customer, error) {
-	return nil, nil
+func (s *dbStore) AllCustomers(db *sqlx.DB) ([]*Customer, error) {
+	customers := []*Customer{}
+
+	err := s.db.Select(&customers, "SELECT * FROM Customers")
+	return customers, err
 }
--- exercise-006-models/src/models/customer_test.go
+++ exercise-006-models/src/models/customer_test.go
@@ -2,10 +2,116 @@ package models
 
 import (
 	"testing"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestCustomer(t *testing.T) {
-	assert := assert.New(t)
-	assert.True(true)
+type StoreSuite struct {
+	suite.Suite
+
+	store  *dbStore
+	db     *sqlx.DB
+	assert *assert.Assertions
+}
+
+func (s *StoreSuite) SetupSuite() {
+	connString := "dbname=models sslmode=disable"
+	db, err := sqlx.Open("postgres", connString)
+	if err != nil {
+		s.T().Fatal(err)
+	}
+	s.db = db
+	s.store = &dbStore{db: db}
+}
+
+func (s *StoreSuite) SetupTest() {
+	_, err := s.store.db.Query("DELETE FROM Customers")
+	if err != nil {
+		s.T().Fatal(err)
+	}
+	s.assert = assert.New(s.T())
+}
+
+func (s *StoreSuite) TearDownSuite() {
+	s.db.Close()
+}
+
+func TestStoreSuite(t *testing.T) {
+	s := new(StoreSuite)
+	suite.Run(t, s)
+}
+
+func (s *StoreSuite) TestNewCustomer() {
+	customer, err := s.store.NewCustomer(s.store.db, "email", "first", "last", time.Now())
+	s.assert.Nil(err)
+	s.assert.NotNil(customer)
+	s.assert.Equal(customer.Email, "email")
+}
+
+func (s *StoreSuite) TestDeleteCustomer() {
+	_, err := s.store.db.Exec(`INSERT INTO customers (email, first_name, last_name, birth_date) VALUES ('test','test','test', NOW());`)
+	s.assert.Nil(err)
+
+	var id int
+	err = s.store.db.QueryRow(`SELECT customer_id FROM Customers WHERE email='test';`).
+		Scan(&id)
+
+	err = s.store.DeleteCustomer(s.store.db, id)
+	s.assert.Nil(err)
+}
+
+func (s *StoreSuite) TestUpdateCustomer() {
+	_, err := s.store.db.Exec(`INSERT INTO customers (email, first_name, last_name, birth_date) VALUES ('test','test','test', NOW());`)
+	s.assert.Nil(err)
+
+	var id int
+	err = s.store.db.QueryRow(`SELECT customer_id FROM Customers WHERE email='test';`).
+		Scan(&id)
+
+	customer, err := s.store.FindCustomerByID(s.store.db, id)
+	s.assert.Nil(err)
+
+	customer.Email = "changed_email"
+
+	err = s.store.UpdateCustomer(s.store.db, customer)
+	s.assert.Nil(err)
+	s.assert.Equal(customer.Email, "changed_email")
+}
+
+func (s *StoreSuite) TestFindCustomerByEmail() {
+	_, err := s.store.db.Exec(`INSERT INTO customers (email, first_name, last_name, birth_date) VALUES ('test_email','test_first','test_last', NOW());`)
+	s.assert.Nil(err)
+
+	customer, err := s.store.FindCustomerByEmail(s.store.db, "test_email")
+	s.assert.Nil(err)
+	s.assert.NotNil(customer)
+	s.assert.Equal(customer.Email, "test_email")
+}
+
+func (s *StoreSuite) TestFindCustomerByID() {
+	_, err := s.store.db.Exec(`INSERT INTO customers (email, first_name, last_name, birth_date) VALUES ('test','test','test', NOW());`)
+	s.assert.Nil(err)
+
+	var id int
+	err = s.store.db.QueryRow(`SELECT customer_id FROM Customers WHERE email='test';`).
+		Scan(&id)
+
+	customer, err := s.store.FindCustomerByID(s.store.db, id)
+	s.assert.Nil(err)
+	s.assert.NotNil(customer)
+	s.assert.Equal(customer.ID, id)
+}
+
+func (s *StoreSuite) TestAllCustomers() {
+	_, err := s.store.db.Exec(`INSERT INTO customers (email, first_name, last_name, birth_date) VALUES ('test','test','test', NOW());`)
+	s.assert.Nil(err)
+
+	customers, err := s.store.AllCustomers(s.store.db)
+	s.assert.Nil(err)
+	s.assert.Equal(len(customers), 1)
+	s.assert.Equal(customers[0].Email, "test")
 }
