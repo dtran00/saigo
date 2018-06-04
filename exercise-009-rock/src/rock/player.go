bbed0fe638bc62a8e30a9c1182c28489804d1908
Deanna <dtran@enova.com>
2018-06-04 09:45:17 -0500

    rock_v1

--- exercise-009-rock/src/rock/player.go
+++ exercise-009-rock/src/rock/player.go
@@ -4,17 +4,67 @@ import (
 	"math/rand"
 )
 
-// Player ...
-type Player struct {
+type Player interface {
+  Type() string
+  Play() int
+}
+
+type RandoRex struct {
 }
 
 // Type returns the type of the player
-func (p *Player) Type() string {
+func (r *RandoRex) Type() string {
 	return "RandoRex"
 }
 
 // Play returns a move
-func (p *Player) Play() int {
+func (r *RandoRex) Play() int {
 	choice := rand.Int() % 3
 	return choice
+}
+
+type Obsessed struct {
+  move int
+}
+
+func (o *Obsessed) Type() string {
+  return "Obsessed"
+}
+
+func (o *Obsessed) Play() int {
+  return o.move
+}
+
+type Flipper struct {
+  move1 int
+  move2 int
+}
+
+func (f *Flipper) Type() string {
+  return "Flipper"
+}
+
+func (f *Flipper) Play() int {
+  choice := rand.Int() % 2
+  if choice == 0 {
+    return f.move1
+  } else {
+    return f.move2
+  }
+}
+
+var prev_choice = 0
+type Cyclone struct {
+}
+
+func (c *Cyclone) Type() string {
+  return "Cyclone"
+}
+
+func (c *Cyclone) Play() int {
+  if prev_choice == 3 {
+    prev_choice = 0
+  }
+  prev_choice += 1
+  return prev_choice
 }
